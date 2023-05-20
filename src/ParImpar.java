78352498ae5bb7fc8a1dfed56358a5332e791361
IAmVinicius <vinicius.costa047@gmail.com>
2023-05-20 09:55:35 -0300

    update

--- src/ParImpar.java
+++ src/ParImpar.java
@@ -6,6 +6,22 @@ import java.util.Scanner;
 public class ParImpar {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int quantosNumeros;
+        int numero = 0;
+        int impar = 0, par = 0;
 
+        System.out.println("Quantos números deseja? ");
+        quantosNumeros = scan.nextInt();
+        int count = 0;
+        do{
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+               if(numero % 2 == 0) par++;
+               else impar ++;
+            count++;
+        }while(count < quantosNumeros);
+
+        System.out.println("Quantidade de números pares: " + par);
+        System.out.println("Quantidade de números impares: " + impar);
     }
 }
