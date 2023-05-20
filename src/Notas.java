4721fc9f02b5fef00626cfe31f515abd7cba4f2d
IAmVinicius <vinicius.costa047@gmail.com>
2023-05-20 09:55:35 -0300

    add new class

---
+++ src/Notas.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Notas {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+        System.out.println("Digite uma nota de 0 a 10: ");
+        nota = scan.nextInt();
+        while(nota < 0 || nota >10){
+            System.out.println("Nota invalida! Digite novamente: ");
+            nota = scan.nextInt();
+        }
+        System.out.println("Parabéns, você digitou a nota " + nota);
+    }
+}
